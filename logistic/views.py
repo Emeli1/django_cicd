3b51f9bde19cf6b6cc8e226fe5349a1af53cc231
Irina Emelianova <irina.m.emelianova@gmail.com>
2025-08-09 13:15:13 +0300

    Update

--- logistic/views.py
+++ logistic/views.py
@@ -1,6 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
