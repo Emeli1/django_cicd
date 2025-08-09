e389ae4f5269684ad33013ab18cf66eaee8d6dfb
Irina Emelianova <irina.m.emelianova@gmail.com>
2025-08-09 12:35:23 +0300

    Initial commit

---
+++ logistic/views.py
@@ -0,0 +1,32 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'description']
+    orderind_fields = ['id']
+    pagination_class = LimitOffsetPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['id']
+    orderind_fields = ['id']
+    pagination_class = LimitOffsetPagination
+
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response({'message': 'Это новая версия!!!'})
